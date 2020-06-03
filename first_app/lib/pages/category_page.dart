0db688c5ffd8c890cea5aefb9c975e4625ddb4a6
tbapman <tbapman@163.com>
2020-06-03 15:19:42 +0800

    flutter first_app first commit

---
+++ first_app/lib/pages/category_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatelessWidget {
+  const CategoryPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('分类首页')));
+  }
+}
---
+++ first_app/lib/pages/member_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MemberPage extends StatelessWidget {
+  const MemberPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('会员')));
+  }
+}
